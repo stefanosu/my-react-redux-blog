6f98d8b3e94dbfe3ee5bb065b2c354af4756f227
Stefanos <stefanosu422@gmail.com>
2020-05-03 03:28:28 -0400

    added app.js and provider component

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'; 
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { };
+    }
+
+    componentWillMount() {
+        store.subscribe(() => this.setState( store.getState() ));
+    }
+
+  render() {
+    const onClick = () => store.dispatch({type: 'TOGGLE'});
+    return (
+        <div>
+          <h1>Hello, World!</h1>
+          <div>
+            Learn Redux 
+            <input 
+                type="checkbox"
+                checked={!!this.state.checked}
+                onClick={onClick}
+            />
+          </div>
+          {
+            this.state.checked ? (<h2>Done!</h2>) : null
+        }   
+        </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,48 +1,25 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 
-const defaultState = { checked: false };
-  const reducer = function(state = defaultState, action) {
-  switch (action.type) {
-    case 'TOGGLE':
-      return { ...state, checked: !state.checked };
-  }
-  return state;
-};
   const store = createStore(reducer);
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+  const defaultState = {
+    checked: false,
+    appName: 'conduit',
+    articles: null
+    };
 
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-  }
+  const reducer = function(state = defaultState, action) {
+      return state;
+    };
 
-  render() {
-    const onClick = () => store.dispatch({type: 'TOGGLE'})
-    return (
-      <div>
-        <h1>To-dos</h1>
-        <div>
-          Learn Redux&nbsp;
-          <input
-            type="checkbox"
-            checked={!!this.state.checked} 
-            onClick={onClick}/>
-          />
-        </div>
-        {
-          this.state.checked ? (<h2>Done!</h2>) : null
-        }
-      </div>
-    );
-  }
+  const store = createStore(reducer) 
 
 ReactDOM.render((
-  <App />
+  <Provider store={store}> 
+    <App />
+  </Provider>
 ), document.getElementById('root'));
