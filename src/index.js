34d7f923e1da11a6637ee80c01a5a80f198e5447
Stefanos <stefanosu422@gmail.com>
2020-05-03 03:04:46 -0400

    added store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()));
+  }
+
   render() {
     return (
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>To-dos</h1>
+        <div>
+          Learn Redux&nbsp;
+          <input
+            type="checkbox"
+            checked={!!this.state.checked} 
+          />
+        </div>
+        {
+          this.state.checked ? (<h2>Done!</h2>) : null
+        }
+      </div>
     );
   }
-}
 
 ReactDOM.render((
   <App />
