67e74847636a9fdfc05c41b63034b405c34f631b
Stefanos <stefanosu422@gmail.com>
2020-05-03 03:17:42 -0400

    added store functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {createStore} from 'redux';
+
+
+const defaultState = { checked: false };
+  const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+  const store = createStore(reducer);
 
 class App extends React.Component {
   constructor() {
@@ -12,6 +24,7 @@ class App extends React.Component {
   }
 
   render() {
+    const onClick = () => store.dispatch({type: 'TOGGLE'})
     return (
       <div>
         <h1>To-dos</h1>
@@ -20,6 +33,7 @@ class App extends React.Component {
           <input
             type="checkbox"
             checked={!!this.state.checked} 
+            onClick={onClick}/>
           />
         </div>
         {
