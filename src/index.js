6d15b6e5b293649c60234b4af9edeadbeb962c08
Stefanos <stefanosu422@gmail.com>
2020-05-06 15:51:51 -0400

    added home page layout and header and banner components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
 
 
   const defaultState = {
