0c03a046fd75890c945581bb8aa92605701da551
Stefanos <stefanosu422@gmail.com>
2020-05-08 21:30:48 -0400

    added react router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store'
+import {Router, Route, IndexRoute, hashHistory } from 'react-router'
+
 
 
 ReactDOM.render((
-  <Provider store={store}> 
-    <App />
+  <Provider store={store}>
+    <Router history={hashHistory}> 
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>
 ), document.getElementById('root'));
