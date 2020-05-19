247ef9945bd5cf0cdf2f382c77af54e91b922c9f
Stefanos <stefanosu422@gmail.com>
2020-05-19 16:10:00 -0400

    added componentWillMount method

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,32 @@ import React from 'react';
 import Header from './Header';
 import Home from './Home'
 import {connect} from 'react-redux';
+import agent from '../agent';
+
 
 const mapStateToProps = state => ({
-  appName: state.common.appName, 
+  appName: state.common.appName,
+  currentUser: state.common.currentUser,
   redirectTo: state.common.redirectTo
 })
 
 const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) => 
+    dispatch({ type: 'APP_LOAD', payload, token}), 
   onRedirectTo: () => {
     dispatch({type: 'REDIRECT'})
   }
 }) 
 
 class App extends React.Component {
+  componentWillMount() {
+    const token = window.localStorage.getItem('jwt')
+    if(token) {
+      agent.setToken(token)
+    }
+    this.props.onLoad(token ? agent.Auth.current() : null, token)
+  }
+
   componentWillReceiveProps(nextProps){
     if(nextProps.redirectTo){
       this.context.router.replace(nextProps.redirectTo)
