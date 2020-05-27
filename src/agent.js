8b59a7c49ee8c66e5c606ceb28947dce9c9050df
Stefanos <stefanosu422@gmail.com>
2020-05-26 22:16:08 -0400

    added register func to auth

--- src/agent.js
+++ src/agent.js
@@ -25,6 +25,9 @@ const Auth = {
     requests.get('/user'),
   login: (email, password) => {
     requests.post('/users/login', { user: { email, password } })
+  }, 
+  register: (username, email, password) => {
+    requests.post('/users', { user: { username, email, password} })
   }
 }
 
