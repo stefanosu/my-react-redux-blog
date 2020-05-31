f2de52372b3b560f79045a61475e49cd1810f394
Stefanos <stefanosu422@gmail.com>
2020-05-30 21:48:58 -0400

    added settings reducer to store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import { promiseMiddleware } from './middleware';
 import auth from './reducers/auth'
 import common from './reducers/common'
 import home from './reducers/home'
+import settings from './reducers/settings';
 
 
   const reducer = combineReducers({
     auth,   
     common,
-    home
+    home,
+    settings
   })
 
   const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose; 
