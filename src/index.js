9627106d53c5f32879861163f4ed8c21d3550e73
Stefanos <stefanosu422@gmail.com>
2020-06-10 15:46:29 -0400

    added settings route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Router, Route, IndexRoute, hashHistory } from 'react-router'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
+import Settings from './components/Settings';
 
 
 
@@ -17,6 +18,7 @@ ReactDOM.render((
       <IndexRoute component={Home}/>
       <Route path="login" component={Login} />
       <Route path="register" component={Register}/> 
+      <Route path="settings" component={Settings}/> 
       </Route>
     </Router>
   </Provider>
