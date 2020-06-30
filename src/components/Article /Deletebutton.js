4d4e4e687d9820e6bf7c4621ba0033d09ba5f3fe
Stefanos <stefanosu422@gmail.com>
2020-06-29 21:50:30 -0400

    added delete button component

--- src/components/Article /Deletebutton.js
+++ src/components/Article /Deletebutton.js
@@ -8,3 +8,20 @@ const mapDispatchToProps = dispatch => ({
     dispatch({ type: 'DELETE_COMMENT', payload, commentId })
 })
 
+const DeleteButton = props => {
+  const del = () => {
+    const payload = agent.Comments.delete(props.slug, props.commentId);
+    props.onClick(payload, props.commentId);
+  };
+
+  if (props.show) {
+    return (
+      <span className="mod-options">
+        <i className="ion-trash-a" onClick={del}></i>
+      </span>
+    );
+  }
+  return null;
+};
+
+export default connect(() => ({}), mapDispatchToProps)(DeleteButton);
