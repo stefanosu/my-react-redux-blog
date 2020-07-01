17dce50d6e6e097c576f8ed765e2ae8f5417277f
Stefanos <stefanosu422@gmail.com>
2020-06-30 20:34:50 -0400

    changed home reducer to return empty obj

--- src/reducers/home.js
+++ src/reducers/home.js
@@ -1,11 +1,7 @@
 export default (state = {}, action) => {
   switch (action.type) {
     case 'HOME_PAGE_LOADED':
-      return {
-        ...state,
-        articles: action.payload.articles
-      };
+      return {}
   }
-
   return state;
 };
