1d543f7d9f2ba3e1e54e1d17d80a298cb58f0f0a
Stefanos <stefanosu422@gmail.com>
2020-07-04 17:57:09 -0400

    added helper components to add editprofilesettings view

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -23,6 +23,52 @@ const mapDispatchToProps = dispatch => ({
   onUnload: () => dispatch({ type: 'PROFILE_PAGE_UNLOADED' })
 });
 
+
+const EditProfileSettings = props => {
+  if (props.isUser) {
+    return (
+      <Link
+        to="settings"
+        className="btn btn-sm btn-outline-secondary action-btn">
+        <i className="ion-gear-a"></i> Edit Profile Settings
+      </Link>
+    );
+  }
+  return null;
+};
+
+const FollowUserButton = props => {
+  if (props.isUser) {
+    return null;
+  }
+
+  let classes = 'btn btn-sm action-btn';
+  if (props.user.following) {
+    classes += ' btn-secondary';
+  } else {
+    classes += ' btn-outline-secondary';
+  }
+
+  const handleClick = ev => {
+    ev.preventDefault();
+    if (props.user.following) {
+      props.unfollow(props.user.username)
+    } else {
+      props.follow(props.user.username)
+    }
+  };
+
+  return (
+    <button
+      className={classes}
+      onClick={handleClick}>
+      <i className="ion-plus-round"></i>
+      &nbsp;
+      {props.user.following ? 'Unfollow' : 'Follow'} {props.user.username}
+    </button>
+  );
+};
+
 class Profile extends React.Component {
   componentWillMount() {
     this.props.onLoad(Promise.all([
